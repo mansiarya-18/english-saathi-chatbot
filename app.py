e329f53c6582b9e8ba8573ca495b3e53399ba850
Mansi Arya <227347302+mansiarya-18@users.noreply.github.com>
2026-06-02 11:27:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from chatbot import get_response
+
+st.title("English Learning Chatbot")
+
+user_input = st.text_input("Type your message")
+
+if st.button("Send"):
+
+    response = get_response(user_input)
+
+    st.write("Bot:", response)
